b9b985efba600fc9be51fb61c2aa7bedac25340d
Antonio Canepa <ajcanepa@gmail.com>
2022-11-10 16:07:41 +0100

    Recuperando el .r

---
+++ codigo.R
@@ -0,0 +1,18 @@
+#comentario :)
+
+#hdjcgkwlsduyhgxfsjbhxkhgshnb
+
+library("dplyr")
+x <- c(1,2,3,4,5,6) 
+y <- c(10,20,30) 
+print(x + y) 
+
+
+# Importación de datos xls.
+library(readxl)
+X48074 <- read_excel("48074.xls")
+View(X48074)
+
+library(readxl)
+X49165 <- read_excel("49165.xls")
+View(X49165)
