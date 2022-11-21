7bf87e7aa75a5fd19b329e9a6fd19a6df0afda1a
EstherIglesias <estheer.iglesiias@gmail.com>
2022-11-21 18:48:42 +0100

    subida de datos mala para poder eliminarlos

--- codigo.R
+++ codigo.R
@@ -10,6 +10,10 @@ library(readxl)
 paro <- read_excel("input/data/paro.xls")
 View(paro)
 
+#Hacemos un resumen para ver en que se diferencian nuestros datos
+summary(comida)
+summary(paro)
+
 # Unión de los datos
 library(tidyverse)
 union <- full_join(x= paro, y=comida)
@@ -18,11 +22,8 @@ union <- full_join(x= paro, y=comida)
 #Acuerdate de hacer pull siempre antes de trabajar por si acaso
 #Tambien de hacer push luego jejeje 
 library(tidyverse)
-esther <- left_join
+esther <- left_join(x= paro, y =comida, by = c("2","3","4","5","6","7","8","9","10"))
 
-#Hacemos un resumen para ver en que se diferencian nuestros datos
-summary()
-summary()
 
 #Debemos hacer un pivot_longer para disminuir el número de columnas y aumentar el número de filas
 %>%
