fcfa5cd4f62cfeabcf94a5774300beeb82d1b11f
EstherIglesias <estheer.iglesiias@gmail.com>
2022-11-21 17:59:46 +0100

    intento de union no se si ha funcionado

--- codigo.R
+++ codigo.R
@@ -12,18 +12,18 @@ View(paro)
 
 # Unión de los datos
 library(tidyverse)
-union <- full_join(x= X49165_1_, y=X48075_2_)
+union <- full_join(x= paro, y=comida)
 
 
 #Acuerdate de hacer pull siempre antes de trabajar por si acaso
 #Tambien de hacer push luego jejeje 
 library(tidyverse)
-esther <- left_join(x= X49165_1_, y=X48075_2_)
+esther <- left_join
 
 #Hacemos un resumen para ver en que se diferencian nuestros datos
-summary(X49165_1_)
-summary(X48075_2_)
+summary()
+summary()
 
 #Debemos hacer un pivot_longer para disminuir el número de columnas y aumentar el número de filas
-X49165_1_%>%
+%>%
   pivot_longer(cols = )
