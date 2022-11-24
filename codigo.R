4274c6047e68f68fe7940fac83c8be56baeee147
Luciasegura <luciasegura02@gmail.com>
2022-11-24 18:16:38 +0100

    probando grafico

--- codigo.R
+++ codigo.R
@@ -30,7 +30,7 @@ vercomoqueda <- full_join(x= ParoxsexoEncolumnas, y=Comidaxsexoencolumnas, by = 
 
 #Relación entre la alimentación y el desempleo.
 library(ggplot2)
-ggplot(data = union, mapping = aes(x=  Mujeres, y = Hombres )) + labs(x="Mujeres", y="Hombres", subtitle="relacion paro y alimentación")+ theme_classic()
+ggplot(data = union, mapping = aes(x=  Mujeres, y = Hombres )) + labs(x="Mujeres", y="De 40 a 44 años", subtitle="relacion paro y alimentación")+ theme_classic()
 
 
 
